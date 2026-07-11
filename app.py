a5f00c91383835a6e1ed039b595d0ceb2c14b08f
Netrakrishnakumar <netrakrishnakumar.official@gmail.com>
2026-07-11 22:00:40 +0530

    Disable reloader in app.run for production

--- app.py
+++ app.py
@@ -177,6 +177,5 @@ def health():
         'ibm_client_initialized': ibm_client is not None
     })
 
-
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=False)
